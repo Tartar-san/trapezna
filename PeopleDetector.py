04ef316baa401a91b890fb7a9c87cdec6e2c70c5
anton.tarasov <Anton.Tarasov@eleks.com>
2017-11-12 16:16:49 +0200

    Initial commit

---
+++ PeopleDetector.py
@@ -0,0 +1,17 @@
+import cv2
+
+class PeopleDetector:
+
+    def __init__(self):
+        pass
+
+    def detect_people(self):
+        """
+
+        :return: list of boundary boxes
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ server.py
@@ -0,0 +1,9 @@
+import flask
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
